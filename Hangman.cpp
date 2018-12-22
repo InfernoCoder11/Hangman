e24320b6cb17620e9c09bc27db206dccca323c2b
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-12-22 19:41:50 +0530

    Forming intial outline

--- Hangman.cpp
+++ Hangman.cpp
@@ -3,12 +3,28 @@
 
 using namespace std;
 
+// Function Prototypes
+int Game();
+
+// Global Variable Declarations
+int NumberOfWords = 0;
+
 int main(){
     srand(time(0)); // Seed rand() with system time
-    char Words[][20] = {"intelligent",
+    char Words[][20] = {"intelligent", // A random word will be selected from this list
                         "unstoppable",
                         "hangman",
                         "silly",
-                        "funny"};
+                        "funny",
+                        };
+    NumberOfWords = sizeof(Words)/sizeof(Words[0]);
+    bool Play = 1;
+    while (Play){
+        Game();
+    }
+    return 0;
+}
+
+int Game(){
     return 0;
 }
