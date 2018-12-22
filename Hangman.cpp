f58ac35a8f77c7a1a7a7bf37578b8e47805ba47b
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-12-22 19:32:09 +0530

    Added srand() and some starting words

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,7 +1,14 @@
 # include <iostream>
+# include <time.h>
 
 using namespace std;
 
-int main(){  
+int main(){
+    srand(time(0)); // Seed rand() with system time
+    char Words[][20] = {"intelligent",
+                        "unstoppable",
+                        "hangman",
+                        "silly",
+                        "funny"};
     return 0;
 }
