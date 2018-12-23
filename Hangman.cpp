5d36d8a75e966e066a1674bf1057a1ff4f3315f6
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-12-23 18:58:09 +0530

    Added random() func and argument to Game()

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,10 +1,12 @@
 # include <iostream>
 # include <time.h>
+# include <string.h>
 
 using namespace std;
 
 // Function Prototypes
-int Game();
+int Game(char Words[][20]);
+int random();
 
 // Global Variable Declarations
 int NumberOfWords = 0;
@@ -20,11 +22,19 @@ int main(){
     NumberOfWords = sizeof(Words)/sizeof(Words[0]);
     bool Play = 1;
     while (Play){
-        Game();
+        Game(Words);
     }
     return 0;
 }
 
-int Game(){
+int Game(char Words[][20]){
+    char Word[20];
+    strcpy(Word, Words[random()]);
     return 0;
+}
+
+int random(){
+    int rnd = rand();
+    rnd = rnd % NumberOfWords;
+    return rnd;
 }
